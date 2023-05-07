e71e17a067585ad68a27fc888312e65d46526b74
viktoriussuwandi <viktorius.tori@gmail.com>
2023-05-07 12:48:47 +0000

    create function to manage screen scale

--- script.js
+++ script.js
@@ -1,26 +1,55 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
-const GAME_WIDTH  = 800;
-const GAME_HEIGHT = 200;
+const GAME_WIDTH    = 800;
+const GAME_HEIGHT   = 200;
+const PLAYER_WIDTH  = 88/1.5;
+const PLAYER_HEIGHT = 94/1.5;
+const max_JUMP_HEIGHT = GAME_HEIGHT;
+const min_JUMP_HEIGHT = 150;
+
+let scaleRatio    = null;
+let previousTime  = null;
+
 
 function setScreen() {
-  sclaRation = getScaleRatio();
-
-}
-
-function getScaleRatio() {
-  const screenHeight = Math.min(
-    window.innerHeight,
-    document.documentElement,
-    clientHeight
-  );
-
-  const screenHeight = Math.min(
-    window.innerWidth,
-    document.documentElement,
-    clientWidth
-  );
-
+  scaleRatio = getScaleRatio();
+  canvas.width  = GAME_WIDTH  * scaleRatio;
+  canvas.height = GAME_HEIGHT * scaleRatio;
   
 }
+
+setScreen();
+window.addEventListener('resize', () => {
+  setTimeout(setScreen,500)
+});
+if(screen.orientation){ screen.orientation.addEventListener("change",setScreen); }
+
+function getScaleRatio() {
+  const screenHeight = Math.min(window.innerHeight,document.documentElement.clientHeight);
+  const screenWidth = Math.min(window.innerWidth,document.documentElement.clientWidth);
+  let window_is_wider = (screenWidth/screenHeight) < (GAME_WIDTH / GAME_HEIGHT)
+  return (window_is_wider ? (screenWidth/GAME_WIDTH) : (screenHeight/GAME_HEIGHT))
+}
+
+function clearScreen() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0,0,canvas.width,canvas.height);
+  
+}
+
+function gameLoop(currentTime) {
+  if(previousTime == null) {
+    previousTime = currentTime;
+    requestAnimationFrame(gameLoop);
+    return;
+  }
+  
+  const frameTimeDelta = currentTime - previousTime;
+  previousTime - currentTime
+  clearScreen();
+  requestAnimationFrame(gameLoop);
+  
+}
+
+requestAnimationFrame(gameLoop);
