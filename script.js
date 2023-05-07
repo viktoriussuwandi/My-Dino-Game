8f07eb4045aceb391ec3dac966caeadfaaa629e0
viktoriussuwandi <viktorius.tori@gmail.com>
2023-05-07 11:13:42 +0000

    initiate script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("hi")
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+const GAME_WIDTH  = 800;
+const GAME_HEIGHT = 200;
+
+function setScreen() {
+  sclaRation = getScaleRatio();
+
+}
+
+function getScaleRatio() {
+  const screenHeight = Math.min(
+    window.innerHeight,
+    document.documentElement,
+    clientHeight
+  );
+
+  const screenHeight = Math.min(
+    window.innerWidth,
+    document.documentElement,
+    clientWidth
+  );
+
+  
+}
