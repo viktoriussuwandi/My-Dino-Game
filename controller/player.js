6eb0d762106f5c0946b59d6139d8670646b48767
viktoriussuwandi <viktorius.tori@gmail.com>
2023-05-07 15:35:21 +0000

    make keyboard controller for dino jump

--- controller/player.js
+++ controller/player.js
@@ -1,7 +1,14 @@
 export default class Player{
+  
   WALK_ANIMATION_TIMER = 200;
   walkAnimationTimer   = this.WALK_ANIMATION_TIMER;
-  dinoRunImages = []
+  dinoRunImages        = [];
+
+  jumpPressed    = false;
+  jumpInProgress = false;
+  falling        = false;
+  JUMP_SPEED     = 0.6;
+  GRAVITY        = 0.4;
   
   constructor(ctx,width,height,minJumpHeight,maxJumpHeight,scaleRatio) {
     this.ctx = ctx;
@@ -14,6 +21,7 @@ export default class Player{
 
     this.x = 10 * scaleRatio
     this.y = this.canvas.height - this.height - (1.5 * scaleRatio)
+    this.yStandingPosition = this.y;
     
     this.standingStillImage = new Image();
     
@@ -28,11 +36,36 @@ export default class Player{
 
     this.dinoRunImages.push(dinoRunImage1);
     this.dinoRunImages.push(dinoRunImage2);
+
+    //Controller keyboard
+    window.addEventListener("keydown", this.keydown);
+    window.addEventListener("keyup",   this.keyup);
+    
+    window.addEventListener("keydown", this.keydown);
+    window.addEventListener("keyup",   this.keyup);
+
+    //controller touch
+    window.addEventListener("touchstart", this.touchStart);
+    window.addEventListener("touchend",   this.touchEnd);
+    
+    window.addEventListener("touchstart", this.touchstart);
+    window.addEventListener("touchend",   this.touchend);
     
   }
 
+  keydown    = (event) => { if(event.code === "Space") { this.jumpPressed = true} }
+  keyup      = (event) => { if(event.code === "Space") { this.jumpPressed = false} }
+  touchstart = () => { this.jumpPressed = true }
+  touchend   = () => { this.jumpPressed = false }
+  
   update(gameSpeed, frameTimeDelta) {
     this.run(gameSpeed, frameTimeDelta);
+    
+    if (this.jumpInProgress) {
+      this.image = this.standingStillImage;
+    }
+    
+    this.jump(frameTimeDelta);
   }
 
   run(gameSpeed, frameTimeDelta) {
@@ -41,6 +74,25 @@ export default class Player{
       this.walkAnimationTimer = this.WALK_ANIMATION_TIMER;
     }
     this.walkAnimationTimer -= frameTimeDelta * gameSpeed;
+  }
+
+  jump(frameTimeDelta) {
+    
+    if (this.jumpPressed) { this.jumpInProgress = true; }
+
+    if (this.jumpInProgress && !this.falling) {
+      if ( (this.y > this.canvas.height - this.minJumpHeight) ||
+         ( (this.y > this.canvas.height - this.maxJumpHeight) && this.jumpPressed)
+      ) { this.y -= this.JUMP_SPEED * frameTimeDelta * this.scaleRatio; } else { this.falling = true; }
+    } else {
+      if (this.y < this.yStandingPosition) {
+        this.y += this.GRAVITY * frameTimeDelta * this.scaleRatio;
+        if ( (this.y + this.height) > this.canvas.height) { this.y = this.yStandingPosition; }
+      } else {
+        this.falling = false;
+        this.jumpInProgress = false;
+      }
+    }
   }
   
   draw() {
